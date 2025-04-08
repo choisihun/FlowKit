593a9ff519aa57ebb34a856c579e8124aae8c916
zI존_sihun <101883606+choisihun@users.noreply.github.com>
2025-04-08 12:54:36 +0900

    Update FlowProviderImpl.swift

--- Sources/FlowKit/Flow/FlowProviderImpl.swift
+++ Sources/FlowKit/Flow/FlowProviderImpl.swift
@@ -3,71 +3,105 @@ import SwiftUI
 @available(iOS 13, *)
 public extension FlowProvider {
   
-  fileprivate func _wrap<C: View>(_ view: C) -> UIViewController {
-    UIHostingController(rootView: view)
-  }
-  
   // MARK: - Push View
   func push<C: View>(_ view: C, animated: Bool = true) {
-    navigationController.pushViewController(_wrap(view), animated: animated)
+    Task {
+      await MainActor.run {
+        navigationController.pushViewController(_wrap(view), animated: animated)
+      }
+    }
   }
   
   // MARK: - Pop View
   func pop(animated: Bool = true) {
-    navigationController.popViewController(animated: animated)
+    Task {
+      await MainActor.run {
+        navigationController.popViewController(animated: animated)
+      }
+    }
   }
   
   // MARK: - Pop View with Specific Count
   func pop(_ count: Int, animated: Bool = true) {
-    let viewControllers = navigationController.viewControllers
-    if count > 0, count < viewControllers.count {
-      let index = viewControllers[viewControllers.count - count]
-      navigationController.popToViewController(index, animated: animated)
+    Task {
+      await MainActor.run {
+        let viewControllers = navigationController.viewControllers
+        let index = viewControllers[viewControllers.count - count]
+        navigationController.popToViewController(index, animated: animated)
+      }
     }
   }
   
   // MARK: - Pop View to Root
   func popToRoot(animated: Bool = true) {
-    navigationController.popToRootViewController(animated: animated)
+    Task {
+      await MainActor.run {
+        navigationController.popToRootViewController(animated: animated)
+      }
+    }
   }
   
   // MARK: - Replace View
   func replace<C: View>(_ views: [C], animated: Bool = true) {
-    let viewControllers = views.map { _wrap($0) }
-    navigationController.setViewControllers(viewControllers, animated: animated)
+    Task {
+      await MainActor.run {
+        let viewControllers = views.map { _wrap($0) }
+        navigationController.setViewControllers(viewControllers, animated: animated)
+      }
+    }
   }
   
   // MARK: - Reload View
   func reload(animated: Bool = false) {
-    let lastViewController = navigationController.topViewController
-    var currentViewControllers: [UIViewController] {
-      navigationController.viewControllers.dropLast()
-    }
-    if let lastViewController {
-      let viewControllers = currentViewControllers + [lastViewController]
-      navigationController.setViewControllers(viewControllers, animated: animated)
+    Task {
+      await MainActor.run {
+        let lastViewController = navigationController.topViewController
+        var currentViewControllers: [UIViewController] {
+          navigationController.viewControllers.dropLast()
+        }
+        if let lastViewController {
+          let viewControllers = currentViewControllers + [lastViewController]
+          navigationController.setViewControllers(viewControllers, animated: animated)
+        }
+      }
     }
   }
   
   // MARK: - Sheet
   func sheet<C: View>(_ view: C, animated: Bool = true) {
-    navigationController.present(_wrap(view), animated: animated)
+    Task {
+      await MainActor.run {
+        navigationController.present(_wrap(view), animated: animated)
+      }
+    }
   }
-  
-  // MARK: - FullScreenCover
-  func fullScreenCover<C: View>(_ view: C, animated: Bool = true) {
-    let viewController = _wrap(view)
-    viewController.modalPresentationStyle = .fullScreen
-    navigationController.present(viewController, animated: animated)
+    
+  // MARK: - FullScreenSheet
+  func fullScreenSheet<C: View>(_ view: C, animated: Bool = true) {
+    Task {
+      await MainActor.run {
+        navigationController.modalPresentationStyle = .fullScreen
+        navigationController.present(_wrap(view), animated: animated)
+        print(navigationController.modalPresentationStyle)
+      }
+    }
   }
 
   // MARK: - Dismiss
-  func dismiss(animated: Bool = true) {
-    navigationController.dismiss(animated: animated)
+  func dismiss(animated: Bool = true, completion: (() -> Void)? = nil) {
+    Task {
+      await MainActor.run {
+        navigationController.dismiss(animated: animated, completion: completion)
+      }
+    }
   }
-  
+    
   // MARK: - Alert
   func alert(_ alert: Alert, animated: Bool = true) {
-    navigationController.present(alert.toAlertController(), animated: animated)
+    Task {
+      await MainActor.run {
+        navigationController.present(alert.toAlertController(), animated: animated)
+      }
+    }
   }
 }
